437ca0a40a3d43351295a796e2b7862b8c7f12d1
7neo7 <52262121+goodgreat1@users.noreply.github.com>
2025-05-09 10:48:04 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+if "page" not in st.session_state:
+    st.session_state.page = "home"
+
+
+if st.session_state.page == "home":
+    st.title("마음 온도계")
+    button_write = st.button('감정 일기 쓰기')
+    button_stress = st.button('간단한 스트레스 테스트')
+    button_graph = st.button('내 데이터 및 통계')
+
+    if button_write:
+        st.session_state.page = 'diary'
+    elif button_stress:
+        st.session_state.page = 'stress'
+    elif button_graph:
+        st.session_state.page = 'analysis'
+
+elif st.session_state.page == "diary":
+    import pages.diary as diary
+    diary.show()
+
+elif st.session_state.page == "stress":
+    import pages.stress as stress
+    stress.show()
+
+elif st.session_state.page == "analysis":
+    import pages.analysis as analysis
+    analysis.show()
